7eb83e7b6f04ce89f1eb15af9e7983d9227fa3b6
moyshik7 <moyshik7@gmail.com>
2025-12-07 09:51:25 +0600

    Tryna fix

--- main.js
+++ main.js
@@ -43,7 +43,8 @@ async function scrapeUrl(page, url) {
     try {
         console.log(`→ Navigating: ${url}`);
         await page.goto(url, {
-            waitUntil: "domcontentloaded",
+            //waitUntil: "domcontentloaded",
+            waitUntil: 'networkidle2',
             timeout: 30000,
         });
 
@@ -76,7 +77,7 @@ async function scrapeUrl(page, url) {
 (async () => {
     const browser = await puppeteer.launch({
         headless: "new",
-        executablePath: '/usr/bin/chromium',
+        //executablePath: '/usr/bin/chromium',
         args: [
             "--no-sandbox",
             "--disable-setuid-sandbox",
